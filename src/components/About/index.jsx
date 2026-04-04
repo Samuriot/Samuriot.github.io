14c36066f7498049d3f08ea6c51a48c6eddcab86
Samuriot <92127962+Samuriot@users.noreply.github.com>
2026-04-04 15:31:22 -0700

    changed cube color scheme & font on About page

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -2,69 +2,75 @@ import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useState } from 'react'
 import {FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngular, faCss3, faGitAlt, faHtml5, faJava, faJsSquare, faSteam, faTwitch, faYoutube } from '@fortawesome/free-brands-svg-icons'
-import { faC } from '@fortawesome/free-solid-svg-icons'
-import LogoS from '../../assets/images/logo-s.png'
+import { faGitAlt, faJava, faJsSquare, faSteam, faTwitch, faYoutube } from '@fortawesome/free-brands-svg-icons'
 import Loader from 'react-loaders'
 
 const About = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
 
     useEffect(() => {
-        return () => setTimeout(() => {
+        const timer = setTimeout(() => {
             setLetterClass('text-animate-hover')
-        }, 4000)  
+        }, 2500)
+
+        return () => clearTimeout(timer)
     }, [])
 
     return (
         <>
         <div className='container about-page'>
-            <div className="text-zone">
-                <h1>
-                    <AnimatedLetters
-                        letterClass={letterClass}
-                        strArray={['A', 'b', 'o', 'u', 't', ' ', 'M', 'e']}
-                        idx={15}
-                    />
-                </h1>
-                <p>I am a third-year Computer Science Major with a Data Science Minor at Seattle U!
-                    I have interests in Backend Development, Game Development and Data Analytics.
-                    Currently, I'm working at Boehringer Ingelheim as an Automation Engineer Intern.
-                </p>
-                <p>
-                    I have prior experience in Java, C++, C#, and Python and am extremely willing to 
-                    learn more about programming and expand my knowledge.
-                </p>
-                <p>
-                    As of right now, I am working on a concert safety mobile application known as Hands Up!
-                    This app is dedicated to spreading knowledge on concert safety, tips, as well as reviews of upcoming concerts!
-                    This app utilizes the Flutter framework to allow for portability and cross-platform support.
-                </p>
-            </div>
+            <div className="about-content">
+                <div className="about-text">
+                    <h1 className="about-heading">
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={['A', 'b', 'o', 'u', 't', ' ', 'M', 'e']}
+                            idx={15}
+                        />
+                    </h1>
+                    <p className="about-copy">
+                        I am a Computer Science major with a Data Science minor at Seattle University, focused on building practical,
+                        reliable software that solves real problems.
+                    </p>
+                    <p className="about-copy">
+                        My strongest interests are backend development, full-stack product work, and clean engineering workflows.
+                        I currently work as an Automation Engineer Intern at Boehringer Ingelheim.
+                    </p>
+                    <p className="about-copy">
+                        I have hands-on experience with Java, C++, C#, Python, and JavaScript. Right now I am building
+                        Hands Up, a concert safety mobile app using Flutter for cross-platform support.
+                    </p>
+                    <div className="focus-list">
+                        <span>Backend APIs</span>
+                        <span>Automation</span>
+                        <span>Data-Oriented Thinking</span>
+                        <span>Mobile Product Work</span>
+                    </div>
+                </div>
 
-            <div className="stage-cube-cont">
-                <div className="cubespinner">
-                    <div className="face1">
-                        <FontAwesomeIcon icon={faJava} color="#DD0031"/>
-                    </div>
-                    <div className="face2">
-                        <FontAwesomeIcon icon={faTwitch} color="#6441A5"/>
-                    </div>
-                    <div className="face3">
-                        <FontAwesomeIcon icon={faSteam} color="#000000"/>
-                    </div>
-                    <div className="face4">
-                        <FontAwesomeIcon icon={faJsSquare} color="#EFD81D"/>
-                    </div>
-                    <div className="face5">
-                        <FontAwesomeIcon icon={faGitAlt} color="#EC4"/>
-                    </div>
-                    <div className="face6">
-                        <FontAwesomeIcon icon={faYoutube} color="#ff0000"/>
+                <div className="stage-cube-cont">
+                    <div className="cubespinner">
+                        <div className="face1">
+                            <FontAwesomeIcon icon={faJava} color="#DD0031"/>
+                        </div>
+                        <div className="face2">
+                            <FontAwesomeIcon icon={faTwitch} color="#6441A5"/>
+                        </div>
+                        <div className="face3">
+                            <FontAwesomeIcon icon={faSteam} color="#000000"/>
+                        </div>
+                        <div className="face4">
+                            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D"/>
+                        </div>
+                        <div className="face5">
+                            <FontAwesomeIcon icon={faGitAlt} color="#EC4"/>
+                        </div>
+                        <div className="face6">
+                            <FontAwesomeIcon icon={faYoutube} color="#ff0000"/>
+                        </div>
                     </div>
                 </div>
             </div>
-
         </div>
         <Loader type="pacman" />
         </>
