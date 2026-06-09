f040753de899042e7ed591c847520b787ee96787
Samuriot <92127962+Samuriot@users.noreply.github.com>
2026-06-09 12:34:48 -0700

    Added new headshot & changed bio

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -3,12 +3,9 @@ import { useEffect, useState } from 'react'
 import AnimatedLetters from "../AnimatedLetters";
 import Loader from 'react-loaders'
 import './index.scss';
+import pfp from '../../assets/images/grad-pic.jpg';
 
 const Home = () => {
-    const [letterClass, setLetterClass] = useState('text-animate')
-    const nameArray = ['J', 'o', 'm', 'i', ' ', 'R', 'u', 'i', 'z']
-    const pfp = "https://media.licdn.com/dms/image/v2/D5603AQHOeL3fYFhTZg/profile-displayphoto-crop_800_800/B56ZhTCdBDHUAM-/0/1753739813067?e=1776902400&v=beta&t=Uzj8b7yb_KcQ7AT0S3ejOGsgj6lW6ox8aZ425aTIyC4";
-
     useEffect(() => {
         const timer = setTimeout(() => {
             setLetterClass('text-animate-hover')
@@ -22,13 +19,13 @@ const Home = () => {
         <div className="container home-page">
             <div className="home-content">
                 <div className="intro-block">
-                    <p className="kicker">Interested in Embedded Systems, Cloud Computing, & DevOps</p>
+                    <p className="kicker">Interested in Systems & Network Engineering + DevOps</p>
                     <h1>
-                        Hi, I&apos;m <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />.
+                        Hi, I&apos;m Jomi Ruiz! 
                     </h1>
                     <p className="summary">
-                     Senior at Seattle University with passion for working on high-traffic systems.
-                     Experience with AI agents, Embedded Systems/Linux OS development, and high throughput systems.  
+                     Seattle University Alumnus with passion for working on high-traffic systems.
+                     Experience with AI agents, Cloud Native & Linux OS development, and high throughput systems.  
                     </p>
                     <div className="actions">
                         <Link to="/project" className="flat-button">VIEW PROJECTS</Link>
@@ -41,7 +38,7 @@ const Home = () => {
                             <img className="profImage" src={pfp} alt="Jomikael Ruiz" />
                         </div>
                     </a>
-                    <p className="role">SDE @ Amazon Games, Incoming SWE @ Oracle</p>
+                    <p className="role">Software Engineer @ Oracle Communications</p>
                 </aside>
             </div>
         </div>
